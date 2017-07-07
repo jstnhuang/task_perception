555bd18bfbbd5dc36c2fffc10c8d8ba805aa8ef4
Justin Huang <jstn@cs.washington.edu>
2017-07-06 18:31:12 -0700

    Moved constants to names.h

---
+++ include/task_perception/names.h
@@ -0,0 +1,13 @@
+#ifndef _PBI_NAMES_H_
+#define _PBI_NAMES_H_
+
+namespace pbi {
+namespace bag {
+const static char kColorTopic[] = "color_in";
+const static char kDepthTopic[] = "depth_in";
+const static char kCameraInfoTopic[] = "camera_info";
+const static char kCameraTransformTopic[] = "camera_transform";
+}
+}  // namespace pbi
+
+#endif  // _PBI_NAMES_H_
