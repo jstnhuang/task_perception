86d7f2fe1ea6e07575f1b4f9f53c4575455c6a6d
Justin Huang <jstn@cs.washington.edu>
2018-01-18 21:42:23 -0800

    Added typedef for KdTree.

--- include/task_perception/pcl_typedefs.h
+++ include/task_perception/pcl_typedefs.h
@@ -1,6 +1,7 @@
 #ifndef _PBI_PCL_TYPEDEFS_H_
 #define _PBI_PCL_TYPEDEFS_H_
 
+#include "pcl/kdtree/kdtree.h"
 #include "pcl/point_cloud.h"
 #include "pcl/point_types.h"
 
@@ -12,5 +13,6 @@ typedef pcl::PointXYZRGB PointC;
 
 typedef pcl::PointCloud<pcl::PointXYZ> PointCloudP;
 typedef pcl::PointCloud<pcl::PointXYZRGB> PointCloudC;
+typedef pcl::KdTree<pcl::PointXYZ> KdTreeP;
 
 #endif  // _PBI_PCL_TYPEDEFS_H_
