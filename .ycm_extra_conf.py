fac5a3521dba7a05ff2856281bf76f72aa4e9319
Justin Huang <jstn@cs.washington.edu>
2018-03-07 13:45:50 -0800

    Added rapid_collision to include list.

--- .ycm_extra_conf.py
+++ .ycm_extra_conf.py
@@ -76,7 +76,9 @@ flags = [
 '-I',
 '../fl/include',
 '-I',
-'../pr2_actions/include'
+'../pr2_actions/include',
+'-I',
+'../rapid_collision/include'
 ]
 
 # Set this to the absolute path to the folder (NOT the file!) containing the
