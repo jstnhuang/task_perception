886e03b6d8d7a65a04f3bae09b93e695f39c44f2
Justin Huang <jstn@cs.washington.edu>
2018-06-19 14:29:18 -0700

    Added query script.

---
+++ task_utils/scripts/queries.py
@@ -0,0 +1,25 @@
+#! /usr/bin/env python
+
+import pprint
+from pymongo import MongoClient
+
+def get_number_of_set_pose_events(db):
+    for demonstration in db.demonstrations.find():
+        num_pose_events = 0
+        num_spawn_events = 0
+        for event in demonstration['events']:
+            if event['type'] == 'set object pose':
+                num_pose_events += 1
+            if event['type'] == 'spawn object':
+                num_spawn_events += 1
+        num_pose_events -= num_spawn_events
+        num_frames = demonstration['frame_count']
+        print '{}\t{}\t{}\t{}%'.format(demonstration['name'], num_pose_events, num_frames, 100.0 * num_pose_events/num_frames )
+
+def main():
+    client = MongoClient()
+    db = client.pbi
+    get_number_of_set_pose_events(db)
+
+if __name__ == '__main__':
+    main()
